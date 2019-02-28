92396e9c3ac3697be75b82803814b6f4ee8eb209
Kenji <fix_91_@hotmail.it>
2019-02-28 18:48:56 +0900

    background setting

---
+++ src/components/Global/BackgroundSection.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+import BackgroundImage from 'gatsby-background-image'
+import "../style.css"
+
+
+export default function BackgroundSection ({img, styleClass, title, children}) {
+  return ( 
+        <BackgroundImage  className={styleClass} fluid={img} >
+            <h1 className="text-center text-white text-uppercase">{title}</h1>
+            {children}
+        </ BackgroundImage>
+  );
+}
+
+BackgroundSection.defaultProps = {
+  ttitle: "default title",
+  styleClass: "default-background"
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,29 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import {FaTrain} from "react-icons/fa"
+import BackgroundSection from "../components/Global/BackgroundSection"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <BackgroundSection img={data.img.childImageSharp.fluid} title="Regular" className="default-background" />
     <Link to="/page-2/">Go to page 2<FaTrain /></Link>
   </Layout>
 )
+
+export const query = graphql`
+  {
+    img:file(relativePath: {eq: "default-background.jpeg"}) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
